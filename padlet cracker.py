3ccffe41c45c1a07891fcac6e92c257a7f4f794a
joon0725 <joon0725@gmail.com>
2022-04-01 15:02:28 +0900

    padlet 담벼락형에서 사용할 수 있는 매크로

--- padlet cracker.py
+++ padlet cracker.py
@@ -13,6 +13,7 @@ url = input("매크로를 돌릴 페이지 URL: ")
 def good_macro():
     a = int(input("좋아요 수"))
     k = int(input("몇번째 글에 좋아요를 달까요?"))
+    print("좋아요 다는 중...")
     for i in range(a):
         s = [chr(random.randint(97, 123)) for _ in range(5)]
         driver.get(url)
@@ -51,11 +52,8 @@ while not done:
         inp = input("랜덤한 스트링을 입력하시려면 r을 아니면 입력할 스트링을 띄어쓰기로 구분하여 입력해주세요\n").split()
         if len(inp) == 1 and inp[0] == 'r':
             mk_post(*mk_rand())
-        elif len(inp) != 1:
-            mk_post(*inp)
         else:
-            print("잘못된 입력입니다")
-            continue
+            mk_post(*inp)
     elif command == 'q':
         print("종료합니다")
         done = True
